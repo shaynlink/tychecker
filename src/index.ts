e73d52a1cdda31fab3b71c4736390b4e51aac83c
shaynlink <guemmoudrayane03@gmail.com>
2023-05-24 19:59:07 +0200

    patch export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './validators/string';
 export * from './validators/object';
 export * from './validators/number';
+export * from './validators/date';
 export * from './regex';
 export * from './type';
