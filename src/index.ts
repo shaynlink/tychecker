356b27a3fae09955b5bb0abd186bea8c41bb5761
shaynlink <guemmoudrayane03@gmail.com>
2023-06-04 20:52:13 +0200

    export datatypechecker

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './validators/string';
 export * from './validators/object';
 export * from './validators/number';
 export * from './validators/date';
+export * from './datatype';
 export * from './regex';
 export * from './type';
